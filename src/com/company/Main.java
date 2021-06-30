233378f9d2c245bfda95a4f6ccf0b79b4f84d47d
JulzV <volkova.julija@inbox.lv>
2021-06-30 18:16:26 +0300

    Homework: parse two string values + operator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// TASK 2. Write a program that adds the value two strings using the wrapper classes to parse the strings. e.g "5" + "8" = 13
+// TASK 2. Write a program that adds the value two strings using the wrapper classes to parse the strings. e.g "5" + "8" = 13
 
-        String number1 = "13";
+//  Initialise variables and operator
+        String number1 = "10";
         String number2 = "7";
-        int result = Integer.parseInt(number1) + Integer.parseInt(number2);
-        System.out.println(number1 + " + " + number2 + " = " + result);
+        String operator = "*";
+        double answer = 0;
+
+//  Print out results
+        System.out.println("Answer: " + number1 + " " + operator + " " + number2 + " = " + calculate(number1, number2, operator));
     }
-}
+
+//  Methode 1: calculation
+        public static double calculate(String number1, String number2, String operator){
+            double answer = 0;
+
+            switch (operator) {
+                case "+":
+                    answer = Double.parseDouble(number1) + Double.parseDouble(number2);
+                    return answer;
+                case "-":
+                    answer = Double.parseDouble(number1) - Double.parseDouble(number2);
+                    return answer;
+                case "*":
+                    answer = Double.parseDouble(number1) * Double.parseDouble(number2);
+                    return answer;
+                case "/":
+                    answer = Double.parseDouble(number1) / Double.parseDouble(number2);
+                    return answer;
+                // if none of the cases worked
+                default:
+                    System.out.println("Enter a valid operator");
+                    break;
+            }
+            return answer;
+
+        }
+    }
+
+
